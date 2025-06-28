0ed21ada5ebf03937348d4469b3a907ce7d12ab5
Furkan Kosacı <101810535+furkankosaci@users.noreply.github.com>
2025-06-28 17:21:37 +0300

    Add files via upload

---
+++ C#101/Odev2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+// Soru - 3: Klavyeden girilen cumle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin
+// elemanlarını sıralayan programı yazınız.
+
+string cumle;
+char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+List<char> bulunanSesliler = new List<char>();
+
+while (true)
+{
+    Console.Write("Bir cumle girin: ");
+    cumle = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(cumle))
+    {
+        Console.WriteLine("Geçersiz giriş. Lütfen bir cumle girin.");
+        continue;
+    }
+
+    string temizcumle = cumle.Replace(" ", "");
+    if (!temizcumle.All(char.IsLetter))
+    {
+        Console.WriteLine("Lütfen sadece harf ve boşluk kullanın.");
+        continue;
+    }
+
+    break;
+}
+
+string kucukcumle = cumle.ToLower();
+
+foreach (char harf in kucukcumle)
+{
+    if (sesliHarfler.Contains(harf))
+    {
+        bulunanSesliler.Add(harf);
+    }
+}
+
+bulunanSesliler.Sort();
+
+Console.WriteLine("Bulunan sesli harfler:");
+foreach (char sesli in bulunanSesliler)
+{
+    Console.WriteLine(sesli);
+}
